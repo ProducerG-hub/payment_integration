88ea26b160fa0c557ed30203361ae35294e41cdf
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-04-18 21:36:45 +0300

    Update: Changing the response type to the limiter message by making it being in JSON format

--- middleware/ratelimiter.js
+++ middleware/ratelimiter.js
@@ -4,12 +4,18 @@ const limiter = require('express-rate-limit');
 module.exports.Pay_rateLimiter = limiter({
     windowMs: 60 * 1000, // 1 minute
     max: 5, 
-    message: 'Too many requests, please try again later.'
+    message: {
+        status:429,
+        message: 'Too many requests, please try again later.'
+    }
 });
 
 // Rate limiter for webhook endpoint which allows a maximum of 100 requests per minute from the same IP address to prevent abuse and ensure fair usage of the webhook processing system.
 module.exports.webhook_rateLimiter = limiter({
     windowMs: 60 * 1000, // 1 minute
     max: 100, 
-    message: 'Too many requests, please try again later.'
+    message: {
+        status:429,
+        message: 'Too many requests, please try again later.'
+    }
 });
