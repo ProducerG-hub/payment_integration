95dfcef48f319fc73005bea614e821365c7f0e87
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-04-18 21:28:07 +0300

    Feature: Adding rate limiting feature to control the traffic of the system by blocking to many requests per particula window

--- middleware/ratelimiter.js
+++ middleware/ratelimiter.js
@@ -1,13 +1,15 @@
 const limiter = require('express-rate-limit');
 
+// Rate limiter for payment-related routes which allows a maximum of 5 requests per minute from the same IP address to prevent abuse and ensure fair usage of the payment processing system.
 module.exports.Pay_rateLimiter = limiter({
     windowMs: 60 * 1000, // 1 minute
-    max: 5, // limit each IP to 5 requests per windowMs
+    max: 5, 
     message: 'Too many requests, please try again later.'
 });
 
+// Rate limiter for webhook endpoint which allows a maximum of 100 requests per minute from the same IP address to prevent abuse and ensure fair usage of the webhook processing system.
 module.exports.webhook_rateLimiter = limiter({
     windowMs: 60 * 1000, // 1 minute
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 100, 
     message: 'Too many requests, please try again later.'
 });
